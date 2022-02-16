47d6df793e4b0867f46f4c318909537aa6a8a00f
Ludvig Lundberg <ludviglundberg@hotmail.com>
2022-02-17 00:57:18 +0100

    fully functioning grid_world

---
+++ grid_world.py
@@ -0,0 +1,45 @@
+class GridWorld:
+    def __init__(self, board, rewards, start, goal, height, width):
+        self.state = board
+        self.rewards = rewards
+        self.actions = ['up','down','left','right']
+        self.actor_pos = start
+        self.goal = goal
+        self._height = height
+        self._width = width
+
+    def current_state(self):
+        return tuple(self.state)
+
+    def act(self, action):
+        self.state[self.actor_pos] = 0
+        if self.out_of_bounds(action):
+            return -5, True
+        match action:
+            case 'up':
+                self.actor_pos = self.actor_pos - self._width
+            case 'down':
+                self.actor_pos = self.actor_pos + self._width
+            case 'left':
+                self.actor_pos = self.actor_pos - 1
+            case 'right':
+                self.actor_pos = self.actor_pos + 1
+        
+        self.state[self.actor_pos] = 1
+
+        value = self.rewards[self.actor_pos] if self.actor_pos in self.rewards else -1
+        if self.actor_pos == self.goal:
+            return value, True
+        else:
+            return value, False
+    
+    def out_of_bounds(self, action):
+        match action:
+            case 'up':
+                return self.actor_pos < self._width
+            case 'down':
+                return self.actor_pos >= len(self.state) - self._width
+            case 'left':
+                return self.actor_pos % self._width == 0
+            case 'right':
+                return self.actor_pos % self._width == self._width - 1
---
+++ qlearning.py
@@ -0,0 +1,49 @@
+from random import randrange
+import numpy as np
+
+class QLearner:
+    def __init__(self, actions, learning_rate = 0.1, discount_factor = 0.9, explore_rate = 0.01):
+        self.actions = actions
+        self.Q = dict()
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
+        self.explore_rate = explore_rate
+        self._action_types = np.array([False, True], dtype=bool)
+
+
+
+    def act(self, state):
+        action = self.policy(state)
+        self.previous_state = state
+        self.previous_action = action
+        return self.actions[action]
+
+    def start_episode(self, initial_state):
+        self.previous_state = initial_state
+        self.previous_action = None
+        if initial_state not in self.Q:
+            self.Q[initial_state] = [0] * len(self.actions)
+
+    def reward(self, reward, state):
+        previous_value = self.Q[self.previous_state][self.previous_action] 
+        temporal_difference = reward + self.discount_factor*self._max_estimated_future_value(state) - previous_value
+        self.Q[self.previous_state][self.previous_action] = previous_value + self.learning_rate * temporal_difference
+
+    def end_episode(self): 
+        self.previous_state = None
+
+    def _max_estimated_future_value(self, state):
+        if state in self.Q:
+            return max(self.Q[state])
+        else:
+            self.Q[state] = [0] * len(self.actions)
+            return 0
+
+    def policy(self, state):
+        possible_actions = self.Q[state]
+        explore = np.random.choice(self._action_types, p=[1-self.explore_rate, self.explore_rate])
+
+        if explore:
+            return randrange(len(possible_actions))
+        else:
+            return possible_actions.index(max(possible_actions))
